b32a2cf21c9483d104b3792606db548a396f6f4c
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2025-12-10 18:00:06 +0545

    Map and Filter code added

---
+++ script.js
@@ -0,0 +1,21 @@
+//Fundamentals of JavaScript
+// Array and Objects
+// Functions and return
+// async js coding
+// Foreach, map, filter, find, indexOf
+
+// Map
+// let arr1 = ["A", "B", "C", "D", "E"]
+// let arr = arr1.map(function(val){
+//     return val;
+// })
+// console.log(arr)
+
+
+// Fitler 
+// let arr = [1, 2, 3, 4, 5, 6]
+// let newArr = arr.filter(function(val){
+//     if(val>3) return true;
+// })
+// let newArr = arr.filter(val => val <= 3)
+// console.log(newArr);
