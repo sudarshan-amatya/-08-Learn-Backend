187682b29b569abd177e6f1dadfe9243d246fda3
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2025-12-15 09:00:23 +0545

    Added reduce

--- script.js
+++ script.js
@@ -5,6 +5,12 @@
 // Foreach, map, filter, find, indexOf
 
 // Foreach doesnot give new array 
+// let colors = ["red", "orange"]
+// let newColor = [];
+// colors.forEach(color => {
+//     newColor.push(color);
+// });
+// console.log(newColor);
 
 // Map
 // let arr1 = ["A", "B", "C", "D", "E"]
@@ -34,6 +40,25 @@
 // let user = users.find(u => u.age > 20);
 // console.log(user);
 
+
+// //Reduce  reduce ma accumulator lai first element diyena vane array ko first element linxa
+// array.reduce((accumulator, currentValue, index, array) => {
+//   return newAccumulator;
+// }, initialValue);
+// const fruits = ['apple', 'banana', 'apple', 'orange', 'banana','mango','apple'];
+
+// const count = fruits.reduce((acc, fruit) => {
+//   acc[fruit]= (acc[fruit] || 0) + 1;
+//   return acc;
+// }, {});
+
+// console.log(count);
+// { apple: 2, banana: 2, orange: 1 }
+
+// const fruits = ['apple', 'banana', 'apple', 'orange', 'banana','mango','apple'];
+// const add = fruits.reduce((acc, fruit) => acc + ', ' + fruit,)
+// console.log(add);
+
 //indexOf
 // array.indexOf(searchElement, fromIndex)
 // const fruits = ["apple", "banana", "cherry", "banana"];
@@ -49,4 +74,5 @@
 //Functions
 //You can get the length of functions, parameters are the length of functions
 //function display(a,b,c,d) then length of function is 4 =a,b,c,d;
-console.log("hello");
+
+
